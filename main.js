ba1834bee86e88f6eed98af1f24b7be57b74a556
Theodor Midtlien <theodor@midtlien.com>
2021-06-17 10:25:57 +0200

    Format main.js

--- main.js
+++ main.js
@@ -1,21 +1,19 @@
 import xapi from 'xapi';
-//import fetch from 'fetch';
-
-//const fetch = require("node-fetch");
 
 function getJSON(){
 
 const url = `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`;
 return xapi.Command.HttpClient.Get({ Url: url })
-.then(r => JSON.parse(r.Body))
-.then(res => res.url)
-
+  .then(r => JSON.parse(r.Body))
+  .then(res => res)
 }
 
 async function init() {
-const JSON = await getJSON();
-console.log(JSON);
-
+  const JSON = await getJSON();
+  const url = JSON.url;
+  const title = JSON.title;
+  const exp = JSON.explanation;
+  console.log(url, title, exp);
 }
-console.log('ferdig')
+
 init();
