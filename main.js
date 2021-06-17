caa0e2ee4b2c868d76304a83e9ec6e61c525bc97
Maria Emine Nylund (mnylund) <mnylund@cisco.com>
2021-06-17 10:49:48 +0200

    added background image in halfwake and camera

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 import xapi from 'xapi';
 
 function getJSON(){
-
 const url = `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`;
 return xapi.Command.HttpClient.Get({ Url: url })
   .then(r => JSON.parse(r.Body))
@@ -13,7 +12,10 @@ async function init() {
   const url = JSON.url;
   const title = JSON.title;
   const exp = JSON.explanation;
-  console.log(url, title, exp);
+  const type = JSON.media_type;
+  console.log(url, title, exp, type);
+  xapi.Command.Cameras.Background.Fetch({Image: User1, Url: url});
+  xapi.Command.UserInterface.Branding.Fetch({Type: "HalfwakeBackground", URL: url});
 }
 
 init();
